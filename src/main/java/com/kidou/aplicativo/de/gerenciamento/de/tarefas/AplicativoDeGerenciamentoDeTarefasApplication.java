d0d79728b96afcbcbff1d351264e51dfc13879bf
K1dou <hique1276@gmail.com>
2024-07-30 03:28:01 -0300

    first commit

---
+++ src/main/java/com/kidou/aplicativo/de/gerenciamento/de/tarefas/AplicativoDeGerenciamentoDeTarefasApplication.java
@@ -0,0 +1,13 @@
+package com.kidou.aplicativo.de.gerenciamento.de.tarefas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AplicativoDeGerenciamentoDeTarefasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AplicativoDeGerenciamentoDeTarefasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kidou/aplicativo/de/gerenciamento/de/tarefas/AplicativoDeGerenciamentoDeTarefasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kidou.aplicativo.de.gerenciamento.de.tarefas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AplicativoDeGerenciamentoDeTarefasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
