001a444182760bf425e8cc93e35a4daddb7858c2
K1dou <hique1276@gmail.com>
2024-07-30 23:05:59 -0300

    campos para login validado

--- src/main/java/com/kidou/aplicativo/de/gerenciamento/de/tarefas/model/dtos/UsuarioLoginDTO.java
+++ src/main/java/com/kidou/aplicativo/de/gerenciamento/de/tarefas/model/dtos/UsuarioLoginDTO.java
@@ -1,8 +1,14 @@
 package com.kidou.aplicativo.de.gerenciamento.de.tarefas.model.dtos;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+
 public class UsuarioLoginDTO {
 
+    @NotEmpty(message = "Informe o Email")
+    @Email(message = "Formato de email invalido")
     private String email;
+    @NotEmpty(message = "Informe sua senha!")
     private String senha;
 
     public String getEmail() {
